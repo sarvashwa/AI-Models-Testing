0567c93a14108575619d434669836058ae836183
Sarvashwa Singh <ssingh6@ntst.com>
2026-06-24 23:46:13 +0530

    Implemented first semantic retrieval pipeline using embeddings and cosine similarity

--- test_retrieval.py
+++ test_retrieval.py
@@ -4,7 +4,8 @@ from bells_chunks import chunks
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-question = "How was speech converted into notes?"
+# question = "How was speech converted into notes?"
+question = "What database was used?"
 
 question_embedding = model.encode(question)
 
@@ -13,12 +14,19 @@ print(f"\nQuestion: {question}\n")
 chunk_embeddings = []
 
 for chunk in chunks:
+    embedChunk = model.encode(chunk)
+    similarity = cos_sim(
+        question_embedding,
+        embedChunk
+    )
     chunk_embeddings.append({
         "text": chunk,
-        "embedding": model.encode(chunk)
+        "embedding": embedChunk,
+        "simialrity": similarity,
     })
 
-similarity = cos_sim(
-    question_embedding,
-    chunk_embeddings
-)
+sorted_chunks = sorted(chunk_embeddings, key=lambda x: x["simialrity"], reverse=True)
+
+for chunk in sorted_chunks:
+    print(f"Chunk: {chunk['text']}")
+    print(f"Similarity: {chunk['simialrity']}\n")
